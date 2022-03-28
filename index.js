296ee3ffacf3f657bfcb2952e483a749be9f3bc8
justine <justine1410@live.fr>
2022-03-28 16:06:33 +0200

    Affichage des données reçu pas l'api

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// const url = 'https://api.openweathermap.org/data/2.5/weather?q='+ ville +'&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric';
+recevoirTempérature(ville="Brignac-la-plaine")
 
 // Demande d'une ville
 let input = document.querySelector('input');
@@ -6,16 +6,35 @@ let btn = document.querySelector('i');
 
 btn.addEventListener('click',()=>{
     let ville = input.value;
+    recevoirTempérature(ville)
+})
+
+function recevoirTempérature(ville){
     const url2 ='https://api.openweathermap.org/data/2.5/forecast?q='+ville +'&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric';
 
     //Récupération des prévisions
     axios.get(url2)
-    .then((response)=>{
-        console.log(response.data);
+    .then((response)=>{     
+        let reponse = response.data
+        console.log(reponse); 
+        let date = reponse.list[0].dt_txt .split(' ');
+        let jour = date[0].split('-').reverse().join('/')
+        let heure = date[1]
+        
+        console.log(jour);   
+           
+        $('.nom-ville').text(reponse.city.name);
+        $('.date').text('Le '+ jour + " à " + heure)
+        $('.temperature').text(reponse.list[0].main.temp + "°");
+        $('.soleil').text(reponse.list[0].weather[0].description)
+        $('.vent').html('<i class="fa-regular fa-flag"></i>' +reponse.list[0].wind.speed + "km/h")
+        $('.min').text(' Minimum : ' + reponse.list[0].main.temp_min + "°");
+        $('.max').text('Maximum : ' + reponse.list[0].main.temp_max + "°");
+
     })
     .catch((error)=>{
         console.log(error);
     })
-})
 
 
+}
